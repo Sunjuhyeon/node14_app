6d79c73b75d2847a7fcd02b40a607b191cc71cba
User-2023QDBISM\Administrator <djandcl@naver.com>
2023-08-22 11:53:28 +0900

    404오류 검수완료

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use('/', banner);
 app.use('/nav', navi);
 app.use('/interview', interview);
 
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname+'/public/nopage.html')
+})
+
 app.listen(8000, () => {
     console.log("8000번 서버구동중")
 })
